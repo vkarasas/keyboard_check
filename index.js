9dd87e5627f4e49d44559027100439e920f8c78f
vkarasas <vkarasas@gmail.com>
2026-01-12 01:11:32 +0200

    delete

--- index.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("keyup", (event) => {
-    const keyName = event.key;
-    const code = event.code;
-    const p = document.createElement("p");
-    p.textContent = `KeyboardEvent: key=${keyName} | code:${code}`
-    document.getElementById("textarea").appendChild(p);
-    window.scrollTo(0, document.body.scrollHeight);
-});
