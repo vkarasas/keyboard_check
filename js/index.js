092b2817442428187c017ebd6fa1e4c36dab0ded
vkarasas <vkarasas@gmail.com>
2026-02-22 22:00:30 +0200

    Make keyboard visible and change border color when is keyup

--- js/index.js
+++ js/index.js
@@ -1,8 +1,21 @@
 window.addEventListener("keyup", (event) => {
     const keyName = event.key;
     const code = event.code;
+    //button function with timeout 100 milleseconds
+    button(code, keyName, false);
+    setTimeout(() => {
+        button(code, keyName, true);
+    }, 100)
+   
     const p = document.createElement("p");
     p.textContent = `KeyboardEvent: key=${keyName} | code:${code}`
     document.getElementById("textarea").appendChild(p);
     window.scrollTo(0, document.body.scrollHeight);
 });
+
+function button(code, keyName, reset) {
+    console.log("code: ", code);
+    console.log("keyName: ", keyName);
+    const border = (!reset) ? '2px solid red': '2px solid black'
+    document.getElementById(code).style.border = border;
+}
